011a6653cf0b81db5f05de472412bcf8b4479c1f
Tom Planche <Tomplanche@icloud.com>
2025-03-07 22:14:39 +0100

    [25] (feat on main) Better config.

--- src/config.rs
+++ src/config.rs
@@ -14,7 +14,7 @@ impl AppConfig {
     pub fn new() -> Self {
         dotenv::dotenv().ok();
 
-        let address = env::var("APP_URL").unwrap_or("127.0.0.1:5775".to_string());
+        let address = env::var("APP_URL").unwrap_or("http://127.0.0.1:5775".to_string());
         let port: i16 = env::var("SERVICE_PORT")
             .unwrap_or("5775".to_string())
             .parse()
@@ -36,8 +36,6 @@ impl Default for AppConfig {
 
 impl From<AppConfig> for Figment {
     fn from(config: AppConfig) -> Self {
-        rocket::Config::figment()
-            .merge(("port", config.port))
-            .merge(("address", config.address))
+        rocket::Config::figment().merge(("port", config.port))
     }
 }
